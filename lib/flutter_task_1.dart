c6c25f9f14bdd484c4270f0a2f2a76c310f45dce
miki0412 <chanmiki08412@gmail.com>
2023-01-28 20:03:30 +0900

    task

---
+++ lib/flutter_task_1.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class FlutterTask1 extends StatelessWidget {
+  const FlutterTask1 ({Key?key}) : super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF000000),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
